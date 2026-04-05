3d41210fff57c4566b1808977bf417c66cf91eb0
Muhammad Mubashir Shafique <muhammadmubashirshafique@gmail.com>
2026-04-06 04:27:06 +0500

    Final Commit

--- 7_Andriod_App/lib/screens/splash_screen.dart
+++ 7_Andriod_App/lib/screens/splash_screen.dart
@@ -1,89 +1,3 @@
-// import 'dart:async';
-// import 'package:flutter/material.dart';
-// import 'dashboard_screen.dart';
-//
-// class SplashScreen extends StatefulWidget {
-//   @override
-//   _SplashScreenState createState() => _SplashScreenState();
-// }
-//
-// class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
-//   late AnimationController _controller;
-//   late Animation<double> _animation;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//
-//     // Logo Animation (Halka sa bada hona)
-//     _controller = AnimationController(
-//       duration: const Duration(seconds: 2),
-//       vsync: this,
-//     )..forward();
-//
-//     _animation = CurvedAnimation(
-//       parent: _controller,
-//       curve: Curves.easeIn,
-//     );
-//
-//     Timer(Duration(seconds: 40), () {
-//       Navigator.of(context).pushReplacement(
-//         MaterialPageRoute(builder: (context) => DashboardScreen()),
-//       );
-//     });
-//   }
-//
-//   @override
-//   void dispose() {
-//     _controller.dispose();
-//     super.dispose();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Container(
-//         width: double.infinity,
-//         decoration: BoxDecoration(
-//           // Gradient jo premium look deta hai
-//           gradient: LinearGradient(
-//             begin: Alignment.topCenter,
-//             end: Alignment.bottomCenter,
-//             colors: [Color(0xFF1A1A1A), Color(0xFF000000)],
-//           ),
-//         ),
-//         child: Center(
-//           child: FadeTransition(
-//             opacity: _animation,
-//             child: ScaleTransition(
-//               scale: _animation,
-//               child: Container(
-//                 decoration: BoxDecoration(
-//                   shape: BoxShape.circle,
-//                   boxShadow: [
-//                     BoxShadow(
-//                       color: Color(0xFFFFD700).withOpacity(0.2), // Halka sa gold glow
-//                       blurRadius: 50,
-//                       spreadRadius: 5,
-//                     ),
-//                   ],
-//                 ),
-//                 child: Image.asset(
-//                   'assets/logo/icon.png',
-//                   width: 200,
-//                   height: 200,
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
-
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'dashboard_screen.dart';
@@ -93,7 +7,8 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _pulseAnimation;
 
@@ -107,9 +22,10 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
       vsync: this,
     )..repeat(reverse: true); // Ise repeat karenge "Breathing" effect ke liye
 
-    _pulseAnimation = Tween<double>(begin: 1.0, end: 1.1).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
-    );
+    _pulseAnimation = Tween<double>(
+      begin: 1.0,
+      end: 1.1,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
 
     // 4 seconds baad next screen (40 seconds bohot zyada tha!)
     Timer(Duration(seconds: 4), () {
@@ -137,7 +53,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
             radius: 1.2,
             colors: [
               Color(0xFF1E1E1E), // Center thora light
-              Colors.black,      // Corners bilkul dark
+              Colors.black, // Corners bilkul dark
             ],
           ),
         ),
